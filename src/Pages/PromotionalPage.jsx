ba7fee3d4b69427e9920bde7083305b86f558216
Nightcoder <newnightcoder@gmail.com>
2022-01-23 01:38:37 +0100

    promo page ready :art:

--- src/Pages/PromotionalPage.jsx
+++ src/Pages/PromotionalPage.jsx
@@ -26,71 +26,73 @@ const PromotionalPage = () => {
   }, []);
 
   return (
-    <div style={{ height: "calc(100vh - 64px)" }} className="w-full relative bg-black text-right font-cabin">
+    <div style={{ height: "calc(100vh - 64px)" }} className="w-full relative bg-black font-cabin">
       <div
         style={{ background: `url("${img1}") no-repeat fixed center/cover` }}
-        className="h-full relative border border-red-500"
+        className="h-full relative flex flex-col items-center justify-between"
       >
-        <h2 className="relative w-full right-10 md:right-40 top-10 text-gray-100 text-right text-6xl">Promotional</h2>
+        <h2 className="relative md:right-12 text-gray-100 capitalize capitalize text-5xl md:text-6xl self-end pt-4 mr-4">
+          Promotional
+        </h2>
         <div
-          // style={{ transform: `translateY(${offsetY * 1.5}px)` }}
-          className="w-96 absolute right-10 md:right-20 top-24 text-gray-100 text-left text-sm pt-8"
+          style={{ backgroundColor: "rgba(0,0,0,.75)" }}
+          className="w-full md:max-w-md relative md:absolute md:right-5 md:top-24 text-gray-100 text-left md:text-right p-8"
         >
           ColorWare's corporate division is designed with you in mind. We open new doors with our groundbreaking ability
           to offer your company a unique custom designed product for any occasion. By using multiple application
-          techniques, we turn everyday items into breathtaking displays of advertising for your company. Contact
-          branding@colorware.com to discuss the nearly limitless possibilities.
+          techniques, we turn everyday items into breathtaking displays of advertising for your company.
+          <br /> Contact branding@colorware.com to discuss the nearly limitless possibilities.
         </div>
-        <ChevronDown className="text-6xl absolute bottom-7 right-24 text-white animate-bounce" />
+        <ChevronDown
+          className="text-6xl absolute top-1/3 right-4 md:top-3/4 md:right-12 text-white animate-bounce"
+          style={{ opacity: `${1 - offsetY}` }}
+        />
       </div>
 
       <div
         style={{ background: `url("${img2}") no-repeat fixed center/cover` }}
-        className="h-full relative border border-red-500"
+        className="h-full relative md:flex md:flex-center md:justify-start md:gap-8"
       >
-        <h2 className="w-48 relative top-10 pl-12 text-gray-900 text-left text-6xl md:whitespace-nowrap">
+        <h2 className="w-full md:w-min relative top-4 left-8 z-10 md:top-8 pt-4 text-black text-left capitalize text-5xl md:text-6xl whitespace-nowrap">
           Cost Effective
         </h2>
-        <div className="max-w-md text-sm z-10 absolute left-0 top-16 left-12 bg-white text-gray-900 text-left pt-4">
+        <div
+          style={{ backgroundColor: width < 768 ? "rgba(250,250,250,.65)" : "transparent" }}
+          className="max-w-md relative md:-top-2 text-gray-900 p-8"
+        >
           When cost and timeliness play a factor in creating promotional products for your company, we offer inexpensive
           ways to advertise. Single color logo application starts at $25. Shorter lead times than full customization. We
           can print directly to a variety of surfaces.
         </div>
       </div>
 
-      <div
-        style={{ background: `url("${img3}") no-repeat fixed center/cover` }}
-        className="h-full relative border border-red-500"
-      >
-        <h2 className="relative w-full right-10 md:right-40 top-10 text-gray-100 text-right text-6xl md:whitespace-nowrap">
+      <div style={{ background: `url("${img3}") no-repeat fixed center/cover` }} className="h-full relative">
+        <h2 className="relative w-full right-5 md:right-40 top-5 text-gray-100 text-right capitalize text-5xl md:text-6xl md:whitespace-nowrap">
           Custom Color Matching
         </h2>
 
         <div
-          // style={{ transform: `translateY(${-offsetY}px)` }}
-          className="max-w-md absolute right-5 md:right-20 bottom-20 text-gray-100 text-right"
+          style={{ backgroundColor: "rgba(0,0,0,.75)" }}
+          className="w-full md:max-w-md absolute bottom-0 md:left-0 md:bottom-1/3 text-gray-100 text-right p-8"
         >
           ColorWare understands the importance of building a brand, which is why we offer custom color matching. We can
           help you express the core of your company on a variety of products. We match paint colors to pantone numbers
-          or physical samples like fabric, wood, or metal.{" "}
+          or physical samples like fabric, wood, or metal.
         </div>
       </div>
 
       <div
         style={{ background: `url("${img4}") no-repeat fixed center/cover` }}
-        className="h-full relative border border-red-500"
+        className="h-full relative flex items-center justify-center md:block"
       >
-        <h2 className="relative w-full right-10 md:right-40 top-10 text-gray-100 text-right text-6xl md:whitespace-nowrap">
-          Art Application
+        <h2 className="relative -top-4 pl-4 md:left-6 md:top-1/3 text-gray-100 capitalize text-5xl md:text-6xl whitespace-nowrap">
+          Art
         </h2>
-        <div
-          // style={{ transform: `translateY(${-offsetY}px)` }}
-          className="max-w-md absolute right-5 md:right-20 bottom-20 text-gray-100 text-right"
-        >
+        <div className="w-full md:max-w-md absolute bottom-0 md:left-0 text-gray-100 text-left p-8">
           ColorWare has the ability to create simple, one-color logos, or large, full-color artwork. So let us help you
           represent your company or team with the highest quality art application techniques. The art application
           process is completed in a temperature controlled clean room. with color corrected light bulbs and air
-          filtration system.{" "}
+          filtration system.
         </div>
       </div>
 
