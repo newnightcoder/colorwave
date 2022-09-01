c51ba56ff5069a511eff1c87c5e7c780b57a665c
Nightcoder <newnightcoder@gmail.com>
2022-09-01 14:23:15 +0200

    404page correction/sourcemap false added on netlify

--- src/Pages/PageNotFound.jsx
+++ src/Pages/PageNotFound.jsx
@@ -1,12 +1,19 @@
-import React from "react";
 import { Link } from "react-router-dom";
 
 const PageNotFound = () => {
   return (
     <div className="h-screen w-screen bg-gray-300 text-gray-100 flex flex-col items-center justify-center space-y-8">
       <h1 className="text-8xl text-bold">404 - page not found</h1>
-      <Link to="/">
-        <div className="py-2 px-3 bg-black text-white shadow-sm">back to ColorWave</div>
+      <Link to="/" className="w-max group absolute inset-x-0 m-auto md:static">
+        <div className="relative">
+          <span
+            className="block border border-black rounded-sm absolute -inset-1 transform transition-all duration-300 -skew-y-6 bg-yellow-300 group-hover:skew-y-3 group-hover:bg-blue-500"
+            aria-hidden="true"
+          ></span>
+          <h1 className="relative text-lg transition-color duration-300 text-black group-hover:text-white px-2 uppercase">
+            ColorWave
+          </h1>
+        </div>
       </Link>
     </div>
   );
